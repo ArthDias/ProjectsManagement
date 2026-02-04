e579d596b87a10da1127dca61834c82a4bedba00
Arthur Dias <fdias.arthur@gmail.com>
2026-02-04 20:22:08 -0300

    feat: create rough structure and domain model for project

---
+++ ProjectManagement.Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Models
+{
+    [Serializable]
+    internal class Project
+    {
+        public string ProjectNumber { get; set; }
+        public string SubprojectNumber { get; set; }
+        public string ProjectName { get; set; }
+        public string CoodinatorName { get; set; }
+        public decimal Balance { get; set; }
+
+        public Project()
+        {
+        }
+
+        public Project(string projectNumber, string subprojectNumber, string projectName, string coodinatorName, decimal balance)
+        {
+            ProjectNumber = projectNumber;
+            SubprojectNumber = subprojectNumber;
+            ProjectName = projectName;
+            CoodinatorName = coodinatorName;
+            Balance = balance;
+        }
+
+    }
+}
